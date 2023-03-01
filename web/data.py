ac02667c2e47764b1ad6da8eb13d14d4be6f52bd
mis <mearlboro@protonmail.com>
2023-03-01 15:08:38 +0000

    Flask website for data collection
    - add data types and validation to data.py
    - flask application in server.py with templates
    - add one video per group (and incomplete videos.csv) for testing in static/videos
    - configure logging

---
+++ web/data.py
@@ -0,0 +1,128 @@
+from enum import Enum
+from typing import List, Optional
+import csv
+import os
+import random
+
+class EnumStr(Enum):
+    @classmethod
+    def from_str(e, s: str):
+        return e[s.upper()]
+
+    def __str__(e):
+        names = e.name.split('_')
+        name = names[0].capitalize()
+        if len(names) > 1:
+            name += " " + " ".join(n.lower() for n in names[1:])
+        return name
+
+    @classmethod
+    def names(e) -> List[str]:
+        return list(e.__members__.keys())
+
+    @classmethod
+    def members(e) -> List['EnumStr']:
+        return list(e.__members__.values())
+
+
+class Gender(EnumStr):
+    NONE = 0
+    MALE = 1
+    FEMALE = 2
+    NON_BINARY = 3
+
+    def prt(e):
+        if e.name == 'NONE':
+            return "Prefer not to say"
+        else:
+            return super().__str__()
+
+class Ethnicity(EnumStr):
+    OTHER = 0
+    WHITE = 1
+    ASIAN = 2
+    BLACK = 3
+    MIXED = 4
+
+    def prt(e):
+        if e.name == 'ASIAN':
+            return "Asian or British Asian"
+        elif e.name == 'BLACK':
+            return "Black, Black British, Carribean, or African"
+        else:
+            return super().__str__()
+
+
+
+class Video():
+    def __init__(self, fname: str, psi: float) -> None:
+        self.fname  = os.path.join("static/videos", fname)
+        self.vid    = fname.split('.')[0]
+        self.psi    = psi
+        self.rating = -1
+
+    @classmethod
+    def random(self, vids: List['Video'], seen: List['Video'] = []) -> Optional['Video']:
+        v = None
+        if len(seen) < len(vids):
+            i = random.randint(0, len(vids) - 1)
+            while vids[i].vid in set([ v.vid for v in seen ]):
+                i = random.randint(0, len(vids) - 1)
+            v = vids[i]
+        return v
+
+    def rate(self, rating: int) -> None:
+        assert(type(rating) is int and 0 < rating <= 10)
+        self.rating = rating
+
+    def save(self) -> None:
+        with open(f"static/data/v_{self.pid}.csv", 'w') as fh:
+            for v in self.videos:
+                fh.write(f"{v.vid},{v.psi},{v.rating}\n")
+            fh.flush()
+
+
+class Library():
+    @staticmethod
+    def all() -> List['Video']:
+        videos = []
+        with open('static/videos.csv') as csvf:
+            csvr = csv.reader(csvf, delimiter = ',')
+            for row in csvr:
+                # A01_07.mp4, -2.3849239
+                videos.append(Video(row[0], float(row[1])))
+        return videos
+
+
+class Participant():
+    def __init__(self,
+        pid: str = '', age: int = -1,
+        gender: 'Gender' = Gender.NONE, ethnicity: 'Ethnicity' = Ethnicity.OTHER
+    ) -> None:
+        self.pid = pid
+        self.age = age
+        self.gender = gender
+        self.ethnicity = ethnicity
+        self.videos : List['Video'] = []
+
+    def update(self,
+        pid: str, age: int, gender: 'Gender', ethnicity: 'Ethnicity'
+    ) -> None:
+        self.pid = pid
+        self.age = age
+        self.gender = gender
+        self.ethnicity = ethnicity
+
+    def rate(self, v: 'Video', rating: int) -> None:
+        v.rate(rating)
+        self.videos.append(v)
+
+    def save(self) -> None:
+        with open(f"static/data/p_{self.pid}.csv", 'w') as fh:
+            fh.write(f"ID,{self.pid}\n")
+            fh.write(f"Age,{self.age}\n")
+            fh.write(f"Gender,{self.gender}\n")
+            fh.write(f"Ethnicity,{self.ethnicity}\n")
+            fh.flush()
+
+
---
+++ web/server.py
@@ -0,0 +1,82 @@
+from datetime import date
+import logging
+import sys, os, socket
+from flask import Flask, render_template, redirect, request, url_for
+
+from data import Ethnicity, Gender, Participant, Video, Library
+
+hostname = socket.gethostname()
+today = date.today().strftime('%Y%m%d')
+
+logging.basicConfig(level = logging.INFO, format = '%(asctime)s %(message)s',
+    datefmt = '%H:%M:%S', filename = f"logs/{hostname}_{today}.log", filemode = 'a')
+
+
+app = Flask(__name__)
+app.debug = True
+
+p = Participant()
+lib = Library.all()
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/participant", methods = [ 'GET', 'POST' ])
+def participant():
+    if request.method == "POST":
+        global p
+        p = Participant(request.form['pid'], request.form['age'],
+            request.form['gender'], request.form['ethnicity'])
+        p.save()
+        logging.info(f"Save participant details for {p.pid}")
+
+        return redirect(url_for("prep"))
+
+    return render_template("participant.html", data = Participant(),
+        ethnicities = Ethnicity.members(), genders = Gender.members())
+
+
+@app.route("/prep")
+def prep():
+    global p
+    if not p.pid:
+        return redirect(url_for("index"))
+
+    return render_template("prep.html")
+
+
+def _getvideo():
+    global p
+    v = Video.random(vids = lib, seen = p.videos)
+    if v:
+        return render_template("video.html", video = v, participant = p)
+    else:
+        try:
+            p.save()
+            logging.info(f"Finished rating for {p.pid}")
+
+            return render_template('done.html', success_text = 'Data saved successfully')
+        except:
+            return render_template('done.html', success_text = 'Issue with saving participant data. Please mention this to experimenters')
+
+
+@app.route("/video", methods = [ 'GET', 'POST' ])
+def video():
+    global p
+    if not p.pid:
+        return redirect(url_for("index"))
+
+    if request.method == 'GET':
+        return _getvideo()
+    else:
+        v = Video(request.form['vid'], request.form['psi'])
+        r = int(request.form['rating'])
+        p.rate(v, r)
+        p.save()
+
+        logging.info(f"Save rating {r} for {p.pid} and {v.vid}")
+        return _getvideo()
+
+if __name__ == '__main__':
+    app.run(debug = True)
